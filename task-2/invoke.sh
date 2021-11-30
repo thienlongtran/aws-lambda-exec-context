93c8030fadfe2cb1c60dee4b24140b6da430cae6
thienlongtran <tltran5@uno.edu>
2021-11-30 17:44:44 -0600

    Completed task 2 code portion

--- task-2/invoke.sh
+++ task-2/invoke.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-aws lambda invoke \
-    --function-name task_2_test \
-    --payload '{"Message": "CSCI 4452 is such a beautiful class!"}' \
-    response.json
+
+invoke_func(){
+    aws lambda invoke \
+    	--function-name task_2_test \
+    	--payload '{"Message": "CSCI 4452 is such a beautiful class!"}' \
+        response.json
+
+    awk 1 response.json >> responses.txt
+}
+
+invoke_func
