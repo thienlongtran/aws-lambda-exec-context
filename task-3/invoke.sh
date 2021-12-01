0397baed210baa648f39a40e9cf474dffe19645f
thienlongtran <tltran5@uno.edu>
2021-11-30 22:22:44 -0600

    Added 3 Lambda functions for incremental seconds

--- task-3/invoke.sh
+++ task-3/invoke.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 
-invoke_func(){
+invoke_func_1(){
     aws lambda invoke \
-    	--function-name task_3_test \
+    	--function-name task_3_test_1_seconds \
     	--payload '{"Message": "CSCI 4452 is such a beautiful class!"}' \
-        response.json
+        response_1.json
 }
 
-invoke_func
+invoke_func_2(){
+    aws lambda invoke \
+    	--function-name task_3_test_2_seconds \
+    	--payload '{"Message": "CSCI 4452 is such a beautiful class!"}' \
+        response_2.json
+}
+
+invoke_func_3(){
+    aws lambda invoke \
+    	--function-name task_3_test_3_seconds \
+    	--payload '{"Message": "CSCI 4452 is such a beautiful class!"}' \
+        response_3.json
+}
+
+invoke_func_1 & invoke_func_2 & invoke_func_3 &
