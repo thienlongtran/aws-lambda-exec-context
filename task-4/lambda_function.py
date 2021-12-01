96f1f3d5aedb0ffc31bd5c2b07e96df549e57ae3
thienlongtran <tltran5@uno.edu>
2021-12-01 03:51:23 -0600

    Completed Task 4 code

---
+++ task-4/lambda_function.py
@@ -0,0 +1,37 @@
+import socket
+import time
+
+isColdStarted = True
+def enter_cold_context(request_time):
+    global isColdStarted
+    isColdStarted = False
+    elapsed_time = time.time() - request_time
+    execution_context = "Cold Started"
+    return elapsed_time, execution_context
+
+def enter_warm_context(request_time):
+    elapsed_time = time.time() - request_time
+    execution_context = "Warm Started"
+    return elapsed_time, execution_context
+
+def lambda_handler(event, context):
+    request_time = time.time()
+    ip_address = socket.gethostbyname(socket.gethostname())
+    
+    #Print Argument and IP Address
+    print("String Argument: " + str(event))
+    print("IP Address: " + ip_address)
+
+    elapsed_time = None
+    execution_context = None
+    if (isColdStarted):
+        elapsed_time, execution_context = enter_cold_context(request_time)
+    else:
+        elapsed_time, execution_context = enter_warm_context(request_time)
+    
+    return {
+        "statusCode": 200,
+        "lambda_ip_address": ip_address,
+        "elapsed_time": elapsed_time,
+        "execution_context": execution_context
+    }
